2683b11a014ceddc4117bf852ac0340317eaa1c5
Michael Parrish <michael@zooniverse.org>
2014-04-25 11:36:51 -0500

    Towards User TTL.  #4

--- lib/cellect/user.rb
+++ lib/cellect/user.rb
@@ -6,10 +6,36 @@ module Cellect
     include Stateful
     
     attr_accessor :name, :seen
+    attr_accessor :ttl, :ttl_timer
     
-    def initialize(name)
+    def initialize(name, ttl: nil)
       self.name = name
       self.seen = DiffSet::RandomSet.new
+      @ttl = ttl
+      subscribe 'User::state_change', :state_changed
+      after(1){ fake_ready } # fake it until data loading is in place
+    end
+    
+    def seen
+      restart_ttl_timer
+      @seen
+    end
+    
+    def fake_ready
+      transition :ready
+    end
+    
+    def state_changed(topic, state)
+      restart_ttl_timer if state == :ready && ttl
+    end
+    
+    def restart_ttl_timer
+      self.ttl_timer ||= after(ttl){ terminate }
+      ttl_timer.reset
+    end
+    
+    def ttl
+      @ttl || 60 * 60 # 1 hour
     end
   end
 end
--- spec/cellect/user_spec.rb
+++ spec/cellect/user_spec.rb
@@ -8,5 +8,30 @@ module Cellect
     it 'should store seen ids' do
       user.seen.should be_a DiffSet::RandomSet
     end
+    
+    it 'should have a default ttl of 1 hour' do
+      user.ttl.should == 3_600 # seconds
+    end
+    
+    it 'should allow custom ttl' do
+      User.new('test', ttl: 123).ttl.should == 123
+    end
+    
+    it 'should not start the timer until data is loaded' do
+      user.bare_object.should_not_receive :restart_ttl_timer
+      user.transition :initializing
+      user.ttl_timer.should be_nil
+    end
+    
+    it 'should start the ttl timer after loading data' do
+      user.bare_object.should_receive(:restart_ttl_timer).and_call_original
+      user.transition :ready
+      user.ttl_timer.offset.should be_within(1).of user.ttl
+    end
+    
+    it 'should reset the ttl timer on activity' do
+      user.bare_object.should_receive :restart_ttl_timer
+      user.seen
+    end
   end
 end
