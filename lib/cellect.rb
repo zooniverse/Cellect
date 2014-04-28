879f4b47315192bd44056dfd8bb299a872b3cb6f
Michael Parrish <michael@zooniverse.org>
2014-04-28 14:51:53 -0500

    Pairwise sets.  Closes #3

--- lib/cellect.rb
+++ lib/cellect.rb
@@ -2,6 +2,11 @@ require 'diff_set'
 require 'celluloid'
 require 'celluloid/autostart'
 
+module DiffSet
+  require 'diff_set/pairwise_random_set'
+  require 'diff_set/pairwise_priority_set'
+end
+
 module Cellect
   require 'cellect/stateful'
   require 'cellect/project'
---
+++ lib/diff_set/pairwise_random_set.rb
@@ -0,0 +1,8 @@
+module DiffSet
+  class PairwiseRandomSet < RandomSet
+    alias_method :_c_subtract, :subtract
+    def subtract(set, limit)
+      _c_subtract(set, 2 * limit).each_slice(2).to_a.reject{ |pair| pair.length != 2 }
+    end
+  end
+end
