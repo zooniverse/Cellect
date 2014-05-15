ea867921ac2de2150b590146bae5e4e7c422b92f
Michael Parrish <michael@zooniverse.org>
2014-05-15 11:48:22 -0500

    NodeSet specs

--- spec/support/cellect_helper.rb
+++ spec/support/cellect_helper.rb
@@ -1,5 +1,12 @@
+require 'timeout'
+
 module CellectHelper
   def pass_until(obj, is: is)
-    Thread.pass until obj.state == is
+    Timeout::timeout(1) do
+      Thread.pass until obj.state == is
+    end
+  rescue => e
+    puts "Timeout waiting for #{ obj.inspect } to be #{ is }"
+    raise e
   end
 end
--- spec/support/shared_examples_for_node_set.rb
+++ spec/support/shared_examples_for_node_set.rb
@@ -1,5 +1,27 @@
 shared_examples_for 'node set' do
-  it 'should connect to zoo keeper'
-  it 'should know the connection state'
-  it 'should accept a connection string'
+  let(:node_set){ Cellect::NodeSet.new }
+  
+  it 'should connect to zoo keeper' do
+    node_set.zk.should be_nil
+    pass_until node_set, is: :ready
+    node_set.zk.should be_connected
+  end
+  
+  it 'should know the connection state' do
+    node_set.state.should be :initializing
+    pass_until node_set, is: :ready
+    node_set.should be_ready
+  end
+  
+  it 'should accept a connection string' do
+    begin
+      pass_until node_set, is: :ready
+      ENV['ZK_URL'] = 'foobar'
+      node_set.send(:zk_url).should == 'foobar'
+      ENV.delete 'ZK_URL'
+      node_set.send(:zk_url).should == 'localhost:2181'
+    ensure
+      ENV['ZK_URL'] = 'localhost:21811'
+    end
+  end
 end
