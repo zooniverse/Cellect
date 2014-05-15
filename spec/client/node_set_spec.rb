31a7a4101fc556975f24572a5ce2e7dc5ef192bb
Michael Parrish <michael@zooniverse.org>
2014-05-15 12:46:28 -0500

    Cleanup after client NodeSet spec

--- spec/client/node_set_spec.rb
+++ spec/client/node_set_spec.rb
@@ -6,15 +6,19 @@ module Cellect::Client
     let(:node_set){ Cellect::Client.node_set }
     
     it 'should update the node list when changing' do
-      pass_until node_set, is: :ready
-      node_set.zk.create '/nodes/node', data: 'foo', mode: :ephemeral_sequential
-      
-      10.times do
-        break if node_set.nodes['node0000000001']
-        Thread.pass
+      begin
+        pass_until node_set, is: :ready
+        node_set.zk.create '/nodes/node', data: 'foo', mode: :ephemeral_sequential
+        
+        10.times do
+          break if node_set.nodes['node0000000001']
+          Thread.pass
+        end
+        
+        node_set.nodes['node0000000001'].should == 'foo'
+      ensure
+        node_set.zk.delete '/nodes/node0000000001'
       end
-      
-      node_set.nodes['node0000000001'].should == 'foo'
     end
   end
 end
