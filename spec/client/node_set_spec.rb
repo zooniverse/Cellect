ea9c76aed571e93f2d71316286b03bb6a5ec3ba2
Michael Parrish <michael@zooniverse.org>
2014-05-15 12:01:11 -0500

    Client and server NodeSet specs

--- spec/client/node_set_spec.rb
+++ spec/client/node_set_spec.rb
@@ -3,7 +3,18 @@ require 'spec_helper'
 module Cellect::Client
   describe NodeSet do
     it_behaves_like 'node set'
+    let(:node_set){ Cellect::Client.node_set }
     
-    it 'should update the node list when changing'
+    it 'should update the node list when changing' do
+      pass_until node_set, is: :ready
+      node_set.zk.create '/nodes/node', data: 'foo', mode: :ephemeral_sequential
+      
+      10.times do
+        break if node_set.nodes['node0000000001']
+        Thread.pass
+      end
+      
+      node_set.nodes['node0000000001'].should == 'foo'
+    end
   end
 end
--- spec/server/node_set_spec.rb
+++ spec/server/node_set_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 module Cellect::Server
   describe NodeSet do
     it_behaves_like 'node set'
+    let(:node_set){ Cellect::Server.node_set.actors.first }
     
-    it 'should register this node'
+    it 'should register this node' do
+      node_set.id.should == 'node0000000000'
+      node_set.zk.get('/nodes/node0000000000').first.should =~ /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
+    end
   end
 end
