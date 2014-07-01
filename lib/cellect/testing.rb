34a479ca91ee82236b68a5afe3655eceef93a1a7
Michael Parrish <michael@zooniverse.org>
2014-07-01 11:37:50 -0500

    Fake ZK for simple testing

---
+++ lib/cellect/testing.rb
@@ -0,0 +1,17 @@
+module Cellect
+  class NodeSet
+    class ZK
+      def initialize(*args)
+        
+      end
+      
+      def children(*args)
+        []
+      end
+      
+      def method_missing(*args, &block)
+        
+      end
+    end
+  end
+end
