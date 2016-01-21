9e89e052765f92c95f5c0bf2dc210cf6a23ff1b3
Michael Parrish <michael@zooniverse.org>
2016-01-21 13:14:19 -0600

    Always assign redis url and prefix it

--- lib/cellect/attention.rb
+++ lib/cellect/attention.rb
@@ -1,5 +1,5 @@
 require 'attention'
 
 Attention.options[:namespace] = ENV.fetch('ATTENTION_NAMESPACE', 'cellect')
-Attention.options[:redis_url] ||= ENV['REDIS_URL'] if ENV['REDIS_URL']
+Attention.options[:redis_url] = ENV['ATTENTION_REDIS_URL'] if ENV['ATTENTION_REDIS_URL']
 Attention.options[:ttl] = ENV.fetch('ATTENTION_TTL', 20).to_i
